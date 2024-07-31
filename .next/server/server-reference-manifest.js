48e9db18e8217a4fff7bd5b56b740dc6fec27212
Pavel Obukhov <cgr6915@gmail.com>
2024-07-31 23:57:46 +0300

    build

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ZgtwoTJ+/ZHBAhxO7uTzgzLgIbRo+PdVjQXr9p5IrFM=\"}"
---
+++ .next/static/chunks/app/layout-df62add15778ae10.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3818:function(n,e,t){Promise.resolve().then(t.t.bind(t,231,23)),Promise.resolve().then(t.t.bind(t,9960,23))},9960:function(){}},function(n){n.O(0,[215,231,971,23,744],function(){return n(n.s=3818)}),_N_E=n.O()}]);
